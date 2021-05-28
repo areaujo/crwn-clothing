21c39c2f1c2ae64c8b7622ae6318caa2cf0d11d5
areaujo <areaujo@gmail.com>
2021-05-27 21:40:44 -0400

    broken homepage in smaller components and styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Diretory from '../../components/directory/directory.component'
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Diretory></Diretory>
+    </div>
+);
+
+export default HomePage;
